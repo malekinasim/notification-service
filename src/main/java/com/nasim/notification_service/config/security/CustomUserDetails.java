25238a42f19738140107a29b182d29b696f05967
maleki.nasim@gmail.com <maleki.nasim@gmail.com>
2026-04-03 10:50:17 +0200

    change class ti mock interface

--- src/main/java/com/nasim/notification_service/config/security/CustomUserDetails.java
+++ src/main/java/com/nasim/notification_service/config/security/CustomUserDetails.java
@@ -1,33 +1,7 @@
 package com.nasim.notification_service.config.security;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
-
-public class CustomUserDetails implements UserDetails {
-    private final  String tenantId;
-
-    public CustomUserDetails(String tenantId){
-        this.tenantId=tenantId;
-    }
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
+public interface CustomUserDetails extends UserDetails {
+    public String getTenantId();
 }
